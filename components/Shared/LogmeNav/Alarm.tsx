92191941d906a31c81d9279b44850037d7f9e502
gwanhun1 <wjdrhksgns7602@gmail.com>
2025-02-23 18:06:20 +0900

    fix: nav 리팩토링

--- components/Shared/LogmeNav/Alarm.tsx
+++ components/Shared/LogmeNav/Alarm.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Alarm = () => {
-  return (
-    <>
-      <div className="p-2 hover:cursor-not-allowed">Update...</div>
-    </>
-  );
+  return <div className="p-2 hover:cursor-not-allowed">Update...</div>;
 };
 
 export default Alarm;
---
+++ components/Shared/LogmeNav/DesktopNavActions.tsx
@@ -0,0 +1,54 @@
+import { Dispatch, SetStateAction } from 'react';
+import * as Shared from 'components/Shared';
+import Link from 'next/link';
+import NavPriofile from './Profile';
+
+const DesktopNavActions = ({
+  isAuthenticated,
+  setAuthority,
+}: {
+  isAuthenticated: boolean;
+  setAuthority: Dispatch<SetStateAction<boolean>>;
+}) => {
+  if (isAuthenticated) {
+    return (
+      <div className="items-center hidden gap-6 tablet:flex">
+        <Link href="/mypage" className="transition-opacity hover:opacity-80">
+          <Shared.LogmeIcon.SettingsIcon alt="설정" width={28} height={28} />
+        </Link>
+
+        <button className="transition-opacity hover:opacity-80">
+          <Shared.LogmeIcon.NotificationIcon
+            alt="알람"
+            width={28}
+            height={28}
+          />
+        </button>
+
+        <NavPriofile setAuthority={setAuthority} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="items-center hidden tablet:flex">
+      <Link href="/">
+        <Shared.LogmeButton
+          variant="classic"
+          size="small"
+          onClick={() => setAuthority(true)}
+        >
+          <Shared.LogmeHeadline
+            type="medium"
+            fontStyle="semibold"
+            style={{ color: '#fff' }}
+          >
+            Join
+          </Shared.LogmeHeadline>
+        </Shared.LogmeButton>
+      </Link>
+    </div>
+  );
+};
+
+export default DesktopNavActions;
