fc95206a9777ec340c4fba7b50e00a86b6b56712
gwanhun1 <wjdrhksgns7602@gmail.com>
2025-03-09 12:42:34 +0900

    fix: vercel 배포 애러 수정

--- pages/SafeHydrate.tsx
+++ pages/SafeHydrate.tsx
@@ -1,19 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export const SafeHydrate = ({ children }: { children: React.ReactNode }) => {
-    const [mounted, setMounted] = useState(false);
-  
-    useEffect(() => {
-      setMounted(true);
-    }, []);
-  
-    if (!mounted) {
-      return null;
-    }
-  
-    return (
-      <div suppressHydrationWarning className="bg-[#fafaff] min-h-screen">
-        {children}
-      </div>
-    );
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
   }
+
+  return (
+    <div suppressHydrationWarning className="bg-[#fafaff] min-h-screen">
+      {children}
+    </div>
+  );
+};
+export default SafeHydrate;
