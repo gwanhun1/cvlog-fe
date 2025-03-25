7b782f8ac506f38c24c0f5824ca027d537790158
gwanhun1 <wjdrhksgns7602@gmail.com>
2025-03-26 02:31:15 +0900

    feature: seo 최적화

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,23 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ko">
       <Head>
-        {/* Viewport meta tag moved to _app.tsx */}
+        <meta charSet="utf-8" />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="CVLog" />
+        
+        {/* 다양한 파비콘 설정 */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/assets/NavLogo.svg" />
+        
+        {/* 검색 엔진에 표시되는 아이콘 설정 */}
+        <meta name="application-name" content="LogMe" />
+        <meta name="msapplication-TileImage" content="/assets/NavLogo.svg" />
+        <meta property="og:image" content="/assets/NavLogo.svg" />
+        
+        <link rel="canonical" href="https://logme.shop" />
       </Head>
       <body>
         <Main />
