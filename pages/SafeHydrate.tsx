850fe7d1fefda1e516bbef1c4baf880097668e36
gwanhun1 <wjdrhksgns7602@gmail.com>
2025-12-15 17:49:06 +0900

    fix: SafeHydrate SSR 렌더링 활성화

--- pages/SafeHydrate.tsx
+++ pages/SafeHydrate.tsx
@@ -1,19 +1,7 @@
-import { useEffect, useState } from 'react';
-
 export const SafeHydrate = ({ children }: { children: React.ReactNode }) => {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
   return (
     <div suppressHydrationWarning className="bg-[#fafaff] min-h-screen">
-      {children}
+      {typeof window === 'undefined' ? children : children}
     </div>
   );
 };
