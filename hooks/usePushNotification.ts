375ab3ed535cc773b263c99836f5b043a1dc422f
gwanhun1 <wjdrhksgns7602@gmail.com>
2026-06-08 22:15:59 +0900

    fix: GSC 인증 태그 업데이트 및 Uint8Array 타입 에러 수정

--- hooks/usePushNotification.ts
+++ hooks/usePushNotification.ts
@@ -4,11 +4,12 @@ import LocalStorage from 'public/utils/Localstorage';
 
 const VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY ?? '';
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)));
+  const arr = new Uint8Array([...rawData].map((c) => c.charCodeAt(0)));
+  return arr.buffer as ArrayBuffer;
 }
 
 export const usePushNotification = () => {
