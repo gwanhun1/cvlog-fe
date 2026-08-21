5f480a50add8d9861eb8e325f3f236bd9fa4e628
gwanhun1 <wjdrhksgns7602@gmail.com>
2026-08-21 15:57:26 +0900

    hotfix: 레이아웃 정렬 수정

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import cn from 'classnames';
 import { useRouter } from 'next/router';
 
 interface Props {
@@ -8,43 +7,22 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   const { pathname } = useRouter();
-  const isNavVisible =
-    !['/', '/article/new'].includes(pathname) &&
-    !pathname.startsWith('/article/modify/');
   const isArticleContent = pathname.startsWith('/article/content/');
+  const isEditor =
+    pathname === '/article/new' || pathname.startsWith('/article/modify/');
+  const isStandalone = pathname === '/login' || isEditor;
+
+  // 로그인은 뷰포트 중앙, 에디터는 전체 폭을 페이지 자체에서 관리한다.
+  // 글 상세는 좁은 독서 폭을, 그 외 화면은 헤더의 max-w-7xl과 같은
+  // 1280px 내부 기준선(container 1440px - 좌우 80px)을 사용한다.
+  const layoutClassName = isStandalone
+    ? undefined
+    : isArticleContent
+      ? 'mx-auto w-full px-5 pb-6 pt-16 tablet:container tablet:px-20 desktop:px-[17.5rem]'
+      : 'mx-auto w-full px-2 pb-8 pt-20 tablet:container tablet:px-20 tablet:pt-24';
 
   return (
-    <div
-      className={cn(
-        {
-          'tablet:px-20 tablet:container pt-20 tablet:pt-24 pb-8':
-            pathname !== '/login' &&
-            !isArticleContent &&
-            !pathname.startsWith('/article/new') &&
-            !pathname.startsWith('/article/modify/'),
-        },
-        {
-          'px-5 pb-6 pt-16 tablet:container tablet:px-20 tablet:pt-16 desktop:px-[17.5rem]':
-            isArticleContent,
-        },
-        {
-          'px-2':
-            !isArticleContent &&
-            pathname !== '/login' &&
-            !pathname.startsWith('/article/new') &&
-            !pathname.startsWith('/article/modify/'),
-        },
-        {
-          'pt-16':
-            pathname !== '/login' &&
-            isNavVisible &&
-            pathname !== '/' &&
-            !isArticleContent,
-        }
-      )}
-    >
-      {children}
-    </div>
+    <div className={layoutClassName}>{children}</div>
   );
 };
 
